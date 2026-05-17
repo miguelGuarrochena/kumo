3a275ee5619edf0de40c0ba765ca8f3c8488e3d2
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-05-17 19:31:04 +0200

    fix: add padding to content

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -26,7 +26,7 @@ const AppLayout = async ({ children }: { children: React.ReactNode }) => {
       <div className="flex-1 flex flex-col min-w-0 relative">
         <MobileHeader userEmail={user.email ?? ''} />
         <DesktopTopBar />
-        <main className="flex-1 flex flex-col pb-20 lg:pb-0">
+        <main className="flex-1 flex flex-col pb-20 lg:pb-0 pt-4 lg:pt-10">
           <div className="flex-1 w-full max-w-6xl mx-auto p-4 sm:p-6 lg:p-8">{children}</div>
           <Footer variant="app" />
         </main>
